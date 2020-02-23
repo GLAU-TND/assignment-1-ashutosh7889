60995c40aaceef4755b357dea3f29bbb018d9a94
ashu <ashutosh.kuma_cs18>
2020-02-23 23:36:59 +0530

    getter method for referential variable

--- src/node/Node.java
+++ src/node/Node.java
@@ -4,11 +4,15 @@ public class Node<T> {
     private T data;         //Data of node
     private Node<T> next;
 
-    public T getData() {
+    public T getData() {    //getter method
         return data;
     }
 
-    public void setData(T data) {
+    public void setData(T data) {   //setter method
         this.data = data;
+    }
+
+    public Node<T> getNext() {  //getter method for referential variable
+        return next;
     }
 }
