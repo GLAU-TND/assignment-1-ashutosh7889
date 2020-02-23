1702881a3311a98abe017a983a2845774634b11d
ashu <ashutosh.kuma_cs18>
2020-02-24 00:01:04 +0530

    add getter method for head variable

--- src/definition/MyLinkedList.java
+++ src/definition/MyLinkedList.java
@@ -4,6 +4,10 @@ import node.Node;
 
 public class MyLinkedList<T extends Comparable<T>> {
     private Node<T> head;
-    private Node<T> iter;
+    private Node<T> iter;           //use to iterate the list
+
+    public Node<T> getHead() {
+        return head;
+    }
 
 }
