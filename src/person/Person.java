5a37e6a2fc54aede3edb5de30a35fe51feb3a8c6
ashu <ashutosh.kuma_cs18>
2020-02-23 23:47:28 +0530

    add members of private type firstName,lastName,emailID and phoneNumber of string type

---
+++ src/person/Person.java
@@ -0,0 +1,9 @@
+package person;
+
+public class Person implements Comparable<Person> {
+    private String firstName;                   //First name of person
+    private String lastName;                    //Last name of person
+    private String emailID;                     //EmailId of person
+    private StringBuffer phoneNumbers;          //phone number of person
+
+}
