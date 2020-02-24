5d5e4d53eae2628fe84d4cd1945c59a12a2082f9
ashu <ashutosh.kuma_cs18>
2020-02-24 22:01:33 +0530

    create object of class Menu that is menu

--- src/execution/Main.java
+++ src/execution/Main.java
@@ -1,10 +1,13 @@
 package execution;
 
+import definition.Menu;
+
 import java.sql.SQLException;
 
 public class Main {
     public static void main(String[] args) throws SQLException {
         boolean flag = true;
+        Menu menu = new Menu();
 
     }
 }
