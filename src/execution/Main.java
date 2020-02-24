d31d7227bd2073424675a2ac7bcd7dc35afc8806
ashu <ashutosh.kuma_cs18>
2020-02-24 22:02:41 +0530

    add n variable of type integer

--- src/execution/Main.java
+++ src/execution/Main.java
@@ -8,6 +8,8 @@ public class Main {
     public static void main(String[] args) throws SQLException {
         boolean flag = true;
         Menu menu = new Menu();
+        while (flag) {
+            int n = menu.showChoice();
 
+        }
     }
-}
