324365689f83319a72501f8de3a9382be4aded55
ashu <ashutosh.kuma_cs18>
2020-02-24 18:03:22 +0530

    add constructor of class Menu...

--- src/definition/Menu.java
+++ src/definition/Menu.java
@@ -7,5 +7,10 @@ import java.util.Scanner;
 public class Menu {
     private MyLinkedList<Person> linkedList;
     private Scanner scan;
+
+    public Menu() {
+        scan = new Scanner(System.in);
+        linkedList = new MyLinkedList<>();
+    }
 }
 
